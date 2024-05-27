c07ebceaf245aee4c08a2b5fde1129e620e47e4d
yousef2002307 <85695400+yousef2002307@users.noreply.github.com>
2024-05-27 20:13:25 +0300

    First Commit

---
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as Middleware;
+use Closure;
+class VerifyCsrfToken extends Middleware
+{
+    /**
+     * The URIs that should be excluded from CSRF verification.
+     *
+     * @var array<int, string>
+     */
+    protected $except = [
+        //
+    ];
+
+    public function handle($request, Closure $next)
+{
+    $response = $next($request);
+    
+    $response->header('Access-Control-Allow-Origin', '*');
+    $response->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    $response->header('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept, Authorization, X-Requested-With');
+    
+    return $response;
+}
+}
